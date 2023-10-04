2be184545dabdbd9b52192fe4772d6e346248a59
Mariana Téllez <tellezresendizmariana@gmail.com>
2023-10-04 11:52:06 -0600

    Created Data.js component

--- src/CurrencyConverter.js
+++ src/CurrencyConverter.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./CurrencyConverter.css";
+import Data from "./Data";
 
 export default function CurrencyConverter() {
   return (
     <div className="CurrencyConverter">
       <div className="row">
-        <div className="col-12 col-sm-7 left-side ">Left side</div>
+        <div className="col-12 col-sm-7 left-side ">
+          <Data />
+        </div>
         <div className="col-12 col-sm-5 right-side ">Right side</div>
       </div>
     </div>
---
+++ src/Data.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Data() {
+  return <h1>Hey from Data.js</h1>;
+}
