9c35fd3372764134b4163e2868433346bc2d041b
Mariana Téllez <tellezresendizmariana@gmail.com>
2023-10-05 12:30:00 -0600

    Created form

--- src/Data.js
+++ src/Data.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Data() {
-  let amount = 2459;
+  const [amount, setAmount] = useState();
+
   let from = "USD";
   let to = "MXN";
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  function updateAmount(event) {
+    setAmount(event.target.value);
+  }
+
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Enter an amount"
+        onChange={updateAmount}
+      />
+      <input type="submit" value="Submit" />
+    </form>
+  );
 
   return (
     <div className="Data">
@@ -11,6 +31,7 @@ export default function Data() {
       <p>Amount: {amount}</p>
       <p>From: {from}</p>
       <p>To: {to}</p>
+      {form}
     </div>
   );
 }
