76c2e573b6d4620b68b81e393d00b10924b7fbd3
Mariana Téllez <tellezresendizmariana@gmail.com>
2023-10-05 13:14:42 -0600

    Added fields to the form

--- src/Data.js
+++ src/Data.js
@@ -1,17 +1,27 @@
 import React, { useState } from "react";
 
 export default function Data() {
-  const [amount, setAmount] = useState();
-
-  let from = "USD";
-  let to = "MXN";
+  const [data, setData] = useState({
+    amount: 10,
+    from: "USD",
+    to: "MXN"
+  });
 
   function handleSubmit(event) {
     event.preventDefault();
+    console.log(data);
   }
 
   function updateAmount(event) {
-    setAmount(event.target.value);
+    setData({...data, amount: (event.target.value)})
+  }
+
+  function updateFrom(event) {
+    setData({ ...data, from: event.target.value });
+  }
+
+  function updateTo(event) {
+    setData({ ...data, to: event.target.value });
   }
 
   let form = (
@@ -21,6 +31,26 @@ export default function Data() {
         placeholder="Enter an amount"
         onChange={updateAmount}
       />
+      <select
+        value={data.from}
+        onChange={updateFrom}
+        name="currencyFrom"
+        id="currencyFrom"
+      >
+        <option value="yen">YEN</option>
+        <option value="dolar">DOLAR</option>
+        <option value="libra">LIBRA</option>
+      </select>
+      <select
+        value={data.to}
+        onChange={updateTo}
+        name="currencyTo"
+        id="currencyTo"
+      >
+        <option value="yen">YEN</option>
+        <option value="dolar">DOLAR</option>
+        <option value="libra">LIBRA</option>
+      </select>
       <input type="submit" value="Submit" />
     </form>
   );
@@ -28,9 +58,6 @@ export default function Data() {
   return (
     <div className="Data">
       <h2>Get your exchange rate today!</h2>
-      <p>Amount: {amount}</p>
-      <p>From: {from}</p>
-      <p>To: {to}</p>
       {form}
     </div>
   );
