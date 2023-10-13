7c8601c09597f335b3285032400daf383f6e1b92
Mariana Téllez <tellezresendizmariana@gmail.com>
2023-10-13 10:30:01 -0600

    Changed to list and added styling

--- src/DataResults.js
+++ src/DataResults.js
@@ -1,12 +1,15 @@
 import React from "react";
+import "./DataResults.css";
 
-export default function DataResults({data}) {
+export default function DataResults({ data }) {
   return (
     <div>
       <h1>Data Results Child 2</h1>
-      <h3>Data from Parent Component: {data.amount}</h3>
-      <h3>Data from Parent Component: {data.from}</h3>
-      <h3>Data from Parent Component: {data.to}</h3>
+      <ul>
+        <li>Data from Parent Component: {data.amount}</li>
+        <li>Data from Parent Component: {data.from}</li>
+        <li>Data from Parent Component: {data.to}</li>
+      </ul>
     </div>
   );
 }
