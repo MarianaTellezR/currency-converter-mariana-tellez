a41d4f015c51ec882293db456fc19f247e8d9ccb
Mariana Téllez <tellezresendizmariana@gmail.com>
2023-10-12 18:26:03 -0600

    Added new styling sheet

--- src/DataNoResults.js
+++ src/DataNoResults.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "./DataNoResults.css";
 
 export default function DataNoResults() {
   return (
-    <div>
+    <div className="DataNoResults">
       <h2>Hey from NoResults</h2>
     </div>
   );
