8fbe0f2a23717fbc70dfb2dafd15d87afb0e4ab3
Mariana Téllez <tellezresendizmariana@gmail.com>
2023-10-16 15:32:49 -0600

    Updated DataResults.js heading

--- src/DataResults.js
+++ src/DataResults.js
@@ -4,7 +4,7 @@ import "./DataResults.css";
 export default function DataResults({ data }) {
   return (
     <div>
-      <h1>Data Results Child 2</h1>
+      <h2>Your exchange rate:</h2>
       <ul>
         <li>Data from Parent Component: {data.amount}</li>
         <li>Data from Parent Component: {data.from}</li>
