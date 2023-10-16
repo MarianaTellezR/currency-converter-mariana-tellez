bfc6c1436d480d598bc014332b9647b0728d1267
Mariana Téllez <tellezresendizmariana@gmail.com>
2023-10-16 15:16:52 -0600

    Updated DataNoResults.js

--- src/DataNoResults.js
+++ src/DataNoResults.js
@@ -1,10 +1,23 @@
 import React from "react";
 import "./DataNoResults.css";
+import coins from "./img/coins.png";
 
 export default function DataNoResults() {
   return (
     <div className="DataNoResults">
-      <h2>Hey from NoResults</h2>
+      <img src={coins} alt="Currency" />
+      <h2>📈 Real-Time Exchange Rates 📉 </h2>
+      <p>
+        Stay updated with the latest currency exchange rates from around the
+        globe. Our app provides real-time, accurate, and up-to-date data,
+        ensuring you get the best value for your money.
+      </p>
+      <h2>🌍 Multiple Currencies</h2>
+      <p>
+        Support to a wide range of currencies, often including major and minor
+        ones. This versatility is helpful for international travelers or anyone
+        dealing with foreign currencies.
+      </p>
     </div>
   );
 }
