7302cd530808c376399c06e1ad1a991b47520c0b
Mariana Téllez <tellezresendizmariana@gmail.com>
2023-10-17 12:41:02 -0600

    Installed axios and added API call

--- src/DataResults.js
+++ src/DataResults.js
@@ -1,7 +1,20 @@
 import React from "react";
 import "./DataResults.css";
+import axios from "axios";
 
 export default function DataResults({ data }) {
+  function displayCurrencyData(response) {
+    console.log(response);
+  }
+
+  function getApiInfo() {
+    let apiKey = "fca_live_i6RaT2DvGKzu5u40FhbLUdnt46MqeADYktO9ONtq";
+    let apiUrl = `https://api.freecurrencyapi.com/v1/latest?apikey=${apiKey}`;
+    axios.get(apiUrl).then(displayCurrencyData);
+  }
+
+  getApiInfo();
+
   return (
     <div>
       <h2>Your exchange rate:</h2>
