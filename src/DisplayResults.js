199aa24adebe3a16a0dfc7bdff232589ce05a123
Mariana Téllez <tellezresendizmariana@gmail.com>
2023-10-19 12:50:29 -0600

    Added semantic html

--- src/DisplayResults.js
+++ src/DisplayResults.js
@@ -3,10 +3,20 @@ import React from "react";
 export default function DisplayResults(props) {
   return (
     <div className="DisplayResults">
-      <h3>{props.amount}</h3>
-      <h3>{props.base}</h3>
-      <h3>{props.objective}</h3>
-      <h3>{props.result}</h3>
+      <h1>Your exchange rate</h1>
+
+      <h4>US Dollar</h4>
+      <h3>
+        {props.amount} {props.base}
+      </h3>
+      <p>Flechas Imagen</p>
+      <div className="circle">
+        <h4>Mexican Peso</h4>
+        <h2>
+          {props.result}
+          <small> {props.objective}</small>
+        </h2>
+      </div>
     </div>
   );
 }
