3f6074beab7cadee02582e26fb51d281837f02db
Mariana Téllez <tellezresendizmariana@gmail.com>
2023-10-23 16:03:08 -0600

    Updated and added variables

--- src/DisplayResults.js
+++ src/DisplayResults.js
@@ -3,20 +3,25 @@ import arrows from "./img/arrow.png";
 import "./DisplayResults.css";
 
 export default function DisplayResults(props) {
+  let amount = props.amount;
+  let base = props.base;
+  let objective = props.objective;
+  let result = props.result;
+
   return (
     <div className="DisplayResults">
       <h1>Your exchange rate</h1>
 
       <h4>US Dollar</h4>
       <h3>
-        {props.amount} {props.base}
+        {amount} {base}
       </h3>
       <img src={arrows} alt="Conversion" className="arrows" />
       <div className="square">
         <h4>Mexican Peso</h4>
         <h2>
-          {props.result}
-          <small> {props.objective}</small>
+          {result}
+          <small> {objective}</small>
         </h2>
       </div>
     </div>
