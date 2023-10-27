dbad4d89f01bf970c0b3123519f0f72b53f66b98
Mariana Téllez <tellezresendizmariana@gmail.com>
2023-10-27 10:51:49 -0600

    Removed h1

--- src/Flag.js
+++ src/Flag.js
@@ -5,7 +5,6 @@ export default function Flag(props) {
   return (
     <div className="Flags">
       <img src={require(`./img/flags/${props.flag}.png`)} alt={props.flag} />
-      <h1>{props.flag}</h1>
     </div>
   );
 }
