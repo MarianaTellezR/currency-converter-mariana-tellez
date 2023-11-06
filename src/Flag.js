53614441bce7b7cdbc4290e0e42ea2fc37cbbfe6
Mariana Téllez <tellezresendizmariana@gmail.com>
2023-11-06 09:30:00 -0600

    Updated Flag.js with path

--- src/Flag.js
+++ src/Flag.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Flag.css";
 
 export default function Flag(props) {
+  // Added path 
   return (
     <div className="Flags">
       <img src={require(`./img/flags/${props.flag}.png`)} alt={props.flag} />
